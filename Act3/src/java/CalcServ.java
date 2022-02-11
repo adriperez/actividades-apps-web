8683006168b52f3f151708b2954cd208b58033b1
adriperez <adriyu78@gmail.com>
2022-02-10 19:48:29 -0600

    Add files via upload

---
+++ Act3/src/java/CalcServ.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Adriana Pérez
+ */
+public class CalcServ extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CalcServ</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CalcServ at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        
+        PrintWriter out = response.getWriter();
+        int x,y;
+        String str="";
+        x=Integer.parseInt(request.getParameter("num1"));
+        y=Integer.parseInt(request.getParameter("num2"));
+        str=request.getParameter("operation");
+        
+        if(str.equals("suma"))
+            out.println("<h1>El resultado de la suma es:"+(x+y)+"</h1>");
+        else if(str.equals("resta"))
+            out.println("<h1>El resultado de la resta es:"+(x-y)+"</h1>");
+        else if(str.equals("multiplicar"))
+            out.println("<h1>El resultado de la multiplicacion es:"+(x*y)+"</h1>");
+        else if (str.equals("dividir"))
+            out.println("<h1>El resultado de la suma es:"+(x+y)+"</h1>");
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
